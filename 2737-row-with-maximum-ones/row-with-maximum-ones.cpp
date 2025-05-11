b4e12eaf6c58d882718e4a2183afa737a012d62b
Rakshith D N <156822366+RakshithDN@users.noreply.github.com>
2025-05-12 01:03:04 +0530

    Time: 12 ms (12.92%) | Memory: 64.6 MB (89.76%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -5,16 +5,13 @@ public:
         int m = (n > 0) ? mat[0].size() : 0;
         int cnt_max = 0;
         int index = 0;
-
         for (int i = 0; i < n; i++) {
-            int cnt_ones =
-                count(mat[i].begin(), mat[i].end(), 1); 
+            int cnt_ones =count(mat[i].begin(), mat[i].end(), 1); 
             if (cnt_ones > cnt_max || (cnt_ones == cnt_max && i < index)) {
                 cnt_max = cnt_ones;
                 index = i;
             }
         }
-
         return {index, cnt_max};
     }
 };
