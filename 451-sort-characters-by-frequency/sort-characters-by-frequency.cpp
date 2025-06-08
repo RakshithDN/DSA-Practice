1045239e821a168aaf25b1d603a455071d400128
Rakshith D N <156822366+RakshithDN@users.noreply.github.com>
2025-06-08 11:03:43 +0530

    Time: 5 ms (31.09%) | Memory: 11.6 MB (21.49%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string frequencySort(string s) {
         unordered_map<char,int>freq;
-        for(char c:s){
+        for(auto c:s){
             freq[c]++;
         }
         priority_queue<pair<int,char>>maxHeap;
