85f319bbb247d9c366daaf947eca295d07cc5ae9
Rakshith D N <156822366+RakshithDN@users.noreply.github.com>
2025-10-30 19:21:47 +0530

    Time: 555 ms (95.48%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -3,4 +3,4 @@ select s.user_id,ifnull(round(sum(c.action='confirmed')/count(*),2),0)as confirm
 from signups s
 left join confirmations c
 on s.user_id=c.user_id
-group by user_id
+group by s.user_id
